839f06410cc291f34576c8f582d8366b9a428a9e
LF <lf@LF>
2016-10-18 19:39:27 +0200

    First push

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # StrawpollBot
 Simple python strawpoll.me bot
+
+Need urllib2, html5lib and a http proxy list formated like:
+
+1.163.29.130:8080
+1.165.167.140:8080
+1.165.168.62:8080
+1.165.171.152:8080
---
+++ bot.py
@@ -0,0 +1,53 @@
+import sys
+import time
+import urllib
+import urllib2
+import bs4
+
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+
+def init(id):
+    req = urllib2.Request('http://www.strawpoll.me/%s' % id, headers=headers)
+    content = urllib2.urlopen(req).read()
+    soup = bs4.BeautifulSoup(content, 'html5lib')
+    sectoken = soup.find('input', id='field-security-token')['value']
+    authtoken = soup.find('input', id='field-authenticity-token')['name']
+    opts = soup.find('div', id='field-options').find_all('div')
+    optz = []
+    for i in range(len(opts)):
+        opt = opts[i]
+        name = opt.find('label').get_text()
+        value = opt.find('input')['value']
+        optz.append({'name': name, 'value': value})
+        print '%d:' % (i+1), name, '(%s)' % value
+    opt = optz[int(raw_input('Choice: ')) - 1]
+    form_vals={'security-token':sectoken, authtoken:'', 'options':opt['value']}
+    form_vals = urllib.urlencode(form_vals)
+    return form_vals
+
+def vote_loop(file, form_vals):
+    try:
+        f = open(file, 'r')
+    except Exception:
+        print 'file not found.',
+    for ip in f:
+	print 'trying proxy ip: %s' % ip,
+	proxy = urllib2.ProxyHandler({'http': ip})
+	opener = urllib2.build_opener(proxy)
+	urllib2.install_opener(opener)
+	try:
+            req = urllib2.Request('http://www.strawpoll.me/%s' % id, data=form_vals, headers=headers)
+	    urllib2.urlopen(req, timeout=5)
+            print '\033[92m[OK]\033[0m vote done...\n'
+        except Exception:
+            print '\033[91m[KO]\033[0m not reachable, keep going...\n'
+
+if __name__ == '__main__':
+    if len(sys.argv[1:]):
+        form_vals = init(sys.argv[1])
+        vote_loop(sys.argv[2], form_vals)
+        
+    else:
+        print 'Usage: python %s [strawpoll_id] [proxy_list_file]' % sys.argv[0]
+
